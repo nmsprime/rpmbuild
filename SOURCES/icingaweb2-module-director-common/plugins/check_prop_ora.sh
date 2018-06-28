95409ef8e7d9fceb030401c2acccea3b75c7aef4
Ole Ernst <olebowle@gmx.com>
2018-06-28 16:27:37 +0200

    quote variables properly + unify parameters

--- SOURCES/icingaweb2-module-director-common/plugins/check_prop_ora.sh
+++ SOURCES/icingaweb2-module-director-common/plugins/check_prop_ora.sh
@@ -21,12 +21,12 @@ while getopts ":H:C:" opt; do
 	esac
 done
 
-if [ $(snmpget -O qv -v1 -c $com $ip .1.3.6.1.4.1.6437.1.3.3.2.1.0) -eq '2' ]; then
+if [ $(snmpget -Oqv -v1 -c "$com" "$ip" .1.3.6.1.4.1.6437.1.3.3.2.1.0) -eq '2' ]; then
 	echo "ORA STATUS: Critical ALSC RED ALARM"
 	exit 2
 fi
 
-if [ $(snmpget -O qv -v1 -c $com $ip .1.3.6.1.4.1.6437.1.3.3.2.2.0) -eq '3' ]; then
+if [ $(snmpget -Oqv -v1 -c "$com" "$ip" .1.3.6.1.4.1.6437.1.3.3.2.2.0) -eq '3' ]; then
 	echo "ORA STATUS: Warning ALSC Limit"
 	exit 1
 fi
