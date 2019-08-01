e974207730c3aaf99c2ce3923d577b745dfa25ba
Ole Ernst <olebowle@gmx.com>
2019-08-01 10:20:44 +0200

    add check to determine if configfiles have been generated successfully

---
+++ SOURCES/icingaweb2-module-director-common/plugins/check_latest_configfiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# see https://unix.stackexchange.com/a/356400
+
+cd '/tftpboot'
+for type in cm mta; do
+	comm=$(comm -3 <(printf '%s\0' $type/$type-*.conf | xargs -r0 ls -1q | sort | cut -d'.' -f1) <(printf '%s\0' $type/$type-*.cfg | xargs -r0 ls -1q | sort | cut -d'.' -f1))
+	if [[ "$comm" ]]; then
+		echo "CRITICAL - not all $type configfiles were generated:"
+		echo "$comm"
+		exit 2
+	fi
+
+	old=$(awk '$1 > $3 {print $2}' <(paste -d' ' <(printf '%s\0' $type/$type-*.conf | xargs -r0 stat -c '%Y %n') <(printf '%s\0' $type/$type-*.cfg | xargs -r0 stat -c '%Y %n')))
+	if [[ "$old" ]]; then
+		echo "CRITICAL - outdated $type configfiles:"
+		echo "$old"
+		exit 2
+	fi
+done
+
+echo 'OK'
+exit 0
