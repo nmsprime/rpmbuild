b29be298e9b10de54562dfec223dbc6ff084fee8
Christian Schramm <christian.schramm@roetzer-engineering.com>
2020-09-02 13:04:48 +0200

    TD: Rename Commands and Add Aggregated Service

--- SOURCES/icingaweb2-module-director-common/plugins/clusters.sh
+++ SOURCES/icingaweb2-module-director-common/plugins/clusters.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ "$1" = "online" ]; then
-  output=$(grep "$2" /run/nmsprime/icinga2/clusters_online.csv)
-elif [ "$1" = "power" ]; then
-  output=$(grep "$2" /run/nmsprime/icinga2/clusters_power.csv)
+if [ "$1" = "outage" ]; then
+  output=$(grep "$2" /run/nmsprime/icinga2/outage.csv)
+elif [ "$1" = "proactive" ]; then
+  output=$(grep "$2" /run/nmsprime/icinga2/proactive.csv)
+elif [ "$1" = "outage_all" ]; then
+  output=$(grep "^[[:alpha:]]*|'$2" /run/nmsprime/icinga2/outage_all.csv)
+elif [ "$1" = "proactive_all" ]; then
+  output=$(grep "^[[:alpha:]]*|'$2" /run/nmsprime/icinga2/proactive_all.csv)
 else
   exit 2
 fi
@@ -12,10 +16,8 @@ echo "$output"
 
 if [ $(echo $output | cut -d'|' -f1) = "CRITICAL" ]; then
   exit 2
-fi
-
-if [ $(echo $output | cut -d'|' -f1) = "WARNING" ]; then
+elif [ $(echo $output | cut -d'|' -f1) = "WARNING" ]; then
   exit 1
+else
+  exit 0
 fi
-
-exit 0
