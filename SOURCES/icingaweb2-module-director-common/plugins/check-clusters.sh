26b734bc6d96fde84261729c83dc0b83b281d35c
Christian Schramm <christian.schramm@roetzer-engineering.com>
2020-10-14 14:34:01 +0200

    TD: adjust cluster script to new command output

--- SOURCES/icingaweb2-module-director-common/plugins/check-clusters.sh
+++ SOURCES/icingaweb2-module-director-common/plugins/check-clusters.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 if [ "$1" = "outage" ]; then
-  output=$(grep "$2" /run/nmsprime/icinga2/outage.csv)
+  output=$(grep "^[[:alpha:]]*|'$2" /run/nmsprime/icinga2/outage.csv)
 elif [ "$1" = "proactive" ]; then
-  output=$(grep "$2" /run/nmsprime/icinga2/proactive.csv)
-elif [ "$1" = "outage_all" ]; then
-  output=$(grep "^[[:alpha:]]*|'$2" /run/nmsprime/icinga2/outage_all.csv)
-elif [ "$1" = "proactive_all" ]; then
-  output=$(grep "^[[:alpha:]]*|'$2" /run/nmsprime/icinga2/proactive_all.csv)
+  output=$(grep "^[[:alpha:]]*|'$2" /run/nmsprime/icinga2/proactive.csv)
 else
   exit 2
 fi
 
-echo "$output"
+if [ -z "$output" ]; then
+  echo "OK|'$2'=0;0;0;0;0"
+  exit 0
+else
+  echo "$output"
+fi
 
 if [ $(echo $output | cut -d'|' -f1) = "CRITICAL" ]; then
   exit 2
